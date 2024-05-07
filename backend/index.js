58213035ca3ff1ea56a4cc893948e9fd87fa8d0e
Shivam5278 <st527874@gmail.com>
2024-05-08 00:48:55 +0530

    Added user routes.

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+var cors = require("cors");
+
+const rootRouter = require("./routes/index");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1", rootRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
